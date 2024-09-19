d75ee90977c1d84af61fedd9b0037482e4536351
João Vitor Brustolin Oliveira <oliveira.brustolin@gmail.com>
2024-09-19 14:20:56 -0300

    senai
    senai

---
+++ 19092024/exe2.py
@@ -0,0 +1,6 @@
+x =int(input('Digite um numero par: '))
+while x <= 50:
+    print(x)
+    x = x +2
+else:
+    print('E acabou')
---
+++ 19092024/exemplo7.py
@@ -0,0 +1,7 @@
+x = 10
+
+while x <= 10:
+    print(x)
+    x = x-1
+else:
+    print('Acabou')
