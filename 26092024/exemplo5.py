d3a9c0ebc8f7a1e5404b9f616865ef28da0ef49c
João Vitor Brustolin Oliveira <oliveira.brustolin@gmail.com>
2024-09-26 15:54:25 -0300

    senai
    senai

--- 26092024/exemplo5.py
+++ 26092024/exemplo5.py
@@ -1,6 +1,6 @@
 L= []
 while True:
-    n = int(input('Digite um numero (0 sair): '))
+    n = int(input('Digite um numero (0 para imprimir a lista): '))
     if n == 0:
         break
     L.append(n)
---
+++ 26092024/exemplo6.py
@@ -0,0 +1,9 @@
+L= ['A']
+L.append('B')
+print(L)
+L.extend('c')
+print(L)
+L.append(['d', 'e'])
+print(L)
+L.extend(['f', 'g', 'h'])
+print(L)
